252d08c7932614eb355262b2417574f369a27483
Wellington Cavalcante <wellarts@gmail.com>
2025-09-02 14:52:25 -0300

    feat: Adiciona Google Tag Manager ao cabeçalho do catálogo

--- resources/views/public/catalogo.blade.php
+++ resources/views/public/catalogo.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
+
+<!-- Google Tag Manager -->
+        <script>
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-THLL2GCG');
+        </script>
+<!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
     <title>Catálogo de Produtos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -182,6 +193,10 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THLL2GCG"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Cabeçalho Delicaty -->
     <div class="header-delicaty" style="justify-content: space-between;">
         <div style="display: flex; align-items: center;">
